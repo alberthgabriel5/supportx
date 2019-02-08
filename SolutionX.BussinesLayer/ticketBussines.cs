a184e40bd879a9e854d60390984ed7ad90cb62c2
Luis Cerdas Garita <luis4891@live.com>
2019-02-08 09:52:55 -0600

    cambios

---
+++ SolutionX.BussinesLayer/ticketBussines.cs
@@ -0,0 +1,31 @@
+﻿using SolutionX.DomainEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolutionX.DataAccess;
+using SolutionX.DomainEntities;
+using System.Web;
+
+
+namespace SolutionX.BussinesLayer
+{
+    class ticketBussines
+    {
+        TicketData ticketData = new TicketData();
+        List<Ticket> ticketsList = new List<Ticket>();
+
+        public void CreateTicket(Ticket ticket)
+        {
+            ticketData.CreateTicket(ticket);
+        }
+        public List<Ticket> ViewTicket()
+        {
+            ticketsList=ticketData.ViewTicket();
+            return ticketsList;
+
+        }
+    }
+}
--- SolutionX.DataAccess/TicketData.cs
+++ SolutionX.DataAccess/TicketData.cs
@@ -13,7 +13,8 @@ namespace SolutionX.DataAccess
     public class TicketData
     {
         
-        public void CreateTicket(Ticket ticket) {
+        public void CreateTicket(Ticket ticket)
+        {
             
             SqlConnection connectionSupportX = DataAccess.GetSqlConnectionSupportX();
            
@@ -22,7 +23,7 @@ namespace SolutionX.DataAccess
             {
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@description",ticket.description);
-                cmd.Parameters.AddWithValue("@idCostumer", ticket.costumer.id);
+                cmd.Parameters.AddWithValue("@idCostumer", ticket.idCostumer);
 
                 connectionSupportX.Open();
                 cmd.ExecuteNonQuery();
@@ -32,7 +33,45 @@ namespace SolutionX.DataAccess
             }
                 
         }
+        public List<Ticket> ViewTicket()
+        {
+            List<Ticket> ticketsList = new List<Ticket>();
+            SqlConnection connectionSupportX = DataAccess.GetSqlConnectionSupportX();
 
+            SqlDataReader reader;
+
+            
+
+
+            using (SqlCommand cmd = new SqlCommand("sp_listar_ticket_1", connectionSupportX))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                connectionSupportX.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Ticket ticket = new Ticket();
+                    ticket.idCode = reader["idCode"].ToString();
+                    ticket.description= reader["description"].ToString();
+                    ticket.priority = reader["priority"].ToString();
+                    ticket.stateTicket=reader["stateTicket"].ToString();
+                    ticket.dateCreate= Convert.ToDateTime(reader["datecreate"].ToString());
+                    ticket.idCategory = reader["idCategory"].ToString();
+                    ticket.idCostumer = Convert.ToInt32(reader["idCostumer"].ToString());
+                    ticket.idCoordinator = reader["idCordinator"].ToString();
+                    ticket.idEmployee = reader["idEmployee"].ToString();
+
+                    ticketsList.Add(ticket);
+                }
+                connectionSupportX.Close();
+                Console.WriteLine(ticketsList.Count);
+                for (int i = 0; i < ticketsList.Count; i++)
+                {
+                    Console.WriteLine("LA DESCRIPCION DEL TICKET ES: "+ticketsList[i].description);
+                }
+                return ticketsList;
+            }
+        }
 
 
     }
