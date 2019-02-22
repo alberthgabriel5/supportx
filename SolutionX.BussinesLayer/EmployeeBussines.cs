1a5ed5d03bc077c04d14b017e7677891e24bfc5c
Luis Cerdas Garita <luis4891@live.com>
2019-02-22 10:41:07 -0600

    business

---
+++ SolutionX.BussinesLayer/EmployeeBussines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolutionX.DataAccess;
+using SolutionX.DomainEntities;
+
+namespace SolutionX.BussinesLayer
+{
+    class EmployeeBussines
+    {
+
+        EmployeeDataAccess employeeData = new EmployeeDataAccess();
+        public string VerifyUser(Employee employee)
+        {
+          return employeeData.VerifyUser(employee);
+
+        }
+
+        public void AssignRole(Role role, Employee employee)
+        {
+            employeeData.AssignRole(role,employee);
+
+        }
+
+    }
+        
+}
